a64b9a6dfaf03d9be6b3483f079202bdf5034679
robertlagrasse <robert.lagrasse@gmail.com>
2016-09-04 20:51:22 -0700

    TopFragment creates a reference variable of type Communicator, whcih is the Interface implemented in MainActivity.
    The reference variable, comm, is assigned to the return value of getActivity. MainActivity is what the comm reference now points to.
    
    OnClick is modified to call comm.respond(). This allows us to send a string from the fragment to the MainActivity.

--- app/src/main/java/com/example/android/fragments/TopFragment.java
+++ app/src/main/java/com/example/android/fragments/TopFragment.java
@@ -18,6 +18,11 @@ public class TopFragment extends Fragment implements View.OnClickListener {
     Button button;
     int counter = 0;
 
+    // Create a reference variable comm of type Communicator. Communicator is the
+    // Interface we created and implemented in MainActivity
+
+    Communicator comm;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.top_fragment, container, false);
@@ -26,6 +31,9 @@ public class TopFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        // Assign the comm reference variable to the activity.
+        // This means comm now points to MainActivity
+        comm = (Communicator) getActivity();
         button = (Button) getActivity().findViewById(R.id.tf_button);
     }
 
@@ -34,6 +42,11 @@ public class TopFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         // increment a counter when the button gets clicked.
         counter ++;
+
+        // This calls the respond method in MainActivity, since
+        // comm points to mainActivity. This is how we can pass
+        // data back from a fragment.
+        comm.respond("The button was clicked " + counter + " times.");
     }
 }
 

e0adf803332ca17846d3d79bc9448c97b93f8522
robertlagrasse <robert.lagrasse@gmail.com>
2016-09-04 20:29:18 -0700

    TopFragment.java - setup OnClickListener so the fragment can handle button clicks.

--- app/src/main/java/com/example/android/fragments/TopFragment.java
+++ app/src/main/java/com/example/android/fragments/TopFragment.java
@@ -3,6 +3,7 @@ package com.example.android.fragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +12,11 @@ import android.widget.Button;
 /**
  * Created by robert on 9/4/16.
  */
-public class TopFragment extends Fragment {
+
+// implement View.OnClickListener so the fragment can pickup click events
+public class TopFragment extends Fragment implements View.OnClickListener {
     Button button;
+    int counter = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +27,13 @@ public class TopFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         button = (Button) getActivity().findViewById(R.id.tf_button);
+    }
+
+    @Override
+    // Override method required when we implement View.OnClickListener
+    public void onClick(View view) {
+        // increment a counter when the button gets clicked.
+        counter ++;
     }
 }
 

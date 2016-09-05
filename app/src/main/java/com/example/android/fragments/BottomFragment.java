694619ce4e65f4e3cac9f85a6e6999756c7ddb88
robertlagrasse <robert.lagrasse@gmail.com>
2016-09-04 20:19:46 -0700

    TopFragment.java - build java reference to button in xml  BottomFragment.java - build java reference to text in xml

--- app/src/main/java/com/example/android/fragments/BottomFragment.java
+++ app/src/main/java/com/example/android/fragments/BottomFragment.java
@@ -5,13 +5,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by robert on 9/4/16.
  */
 public class BottomFragment extends Fragment {
+    TextView text;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.bottom_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        text = (TextView) getActivity().findViewById(R.id.bf_text);
     }
 }
--- app/src/main/java/com/example/android/fragments/TopFragment.java
+++ app/src/main/java/com/example/android/fragments/TopFragment.java
@@ -6,14 +6,23 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by robert on 9/4/16.
  */
 public class TopFragment extends Fragment {
+    Button button;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.top_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        button = (Button) getActivity().findViewById(R.id.tf_button);
     }
 }
 

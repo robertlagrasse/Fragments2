a02194d450fffd00d46f0aee470a74705986714c
robertlagrasse <robert.lagrasse@gmail.com>
2016-09-04 20:42:40 -0700

    Build new Communicator Interface, which contains respond() method.
    MainActivity IMPLEMENTS this Interface, which means it must also implement the respond() method.

---
+++ app/src/main/java/com/example/android/fragments/Communicator.java
@@ -0,0 +1,8 @@
+package com.example.android.fragments;
+
+/**
+ * Created by robert on 9/4/16.
+ */
+public interface Communicator {
+    void respond(String data);
+}
--- app/src/main/java/com/example/android/fragments/MainActivity.java
+++ app/src/main/java/com/example/android/fragments/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.android.fragments;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+// Implement the Communicator Interface. Requires implementation of all
+// Communicator methods
+public class MainActivity extends AppCompatActivity implements Communicator{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // This is the method the Communicator Interface requires
+    @Override
+    public void respond(String data) {
+
     }
 }
